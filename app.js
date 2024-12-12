eaaa9c1e0fb4709a3b1732ccb83d0b3b87ecee68
AgustinLPZ <alopezpadilla@unomaha.edu>
2024-12-11 18:50:34 -0600

    Added meeting notes slack bot
    Signed-off-by: AgustinLPZ <alopezpadilla@unomaha.edu>

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.message(/outreachy/i, async ({ message, say, logger }) => {
 });
 
 
-// Sends CHAOSS Meeting Notes Link
+// Sends CHAOSS Meeting Notes Link 
 const meetingNotesLink = process.env.MEETING_NOTES_LINK;
 
 app.message(/(meeting|meeting notes|notes)/i, async ({ message, say }) => {
